a781dd84d72ab6fc06faaf47468020e9a187e734
Tyler Rhodes <tylerlrhodes@gmail.com>
2017-04-29 21:14:50 -0400

    Simple CookieAuth Sample...

---
+++ CookieAuth/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Authentication;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CookieAuth
+{
+
+  public class HomeController : Controller
+  {
+    private ILogger _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+    
+    [Produces("text/html")]
+    public string Index()
+    {
+      return "<a href=\"/home/test\">Test</a> <br /> <a href=\"/home/test2\">Test 2</a>";
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Login(string username, string password)
+    {
+      _logger.LogInformation($"{username}, {password}");
+
+      if (password == "dumb")
+      {
+        var claims = new List<Claim>
+        {
+          new Claim("UserName", "Tyler"),
+          new Claim(ClaimTypes.Name, "TylerRhodes")
+        };
+
+        var identity = new ClaimsIdentity(claims, "TestInstance");
+
+        await HttpContext.Authentication.SignInAsync("TestInstance", new ClaimsPrincipal(identity), new AuthenticationProperties());
+      }
+      return RedirectToAction("Index");
+    }
+
+    [Authorize(Policy = "YouMayPass")]
+    public string Test()
+    {
+      return $"Worked {HttpContext.User.Identity.Name}";
+    }
+
+    [Authorize(Policy = "YouMayNotPass")]
+    public string Test2()
+    {
+      return "Error if you see this!";
+    }
+
+    public string FailedAccess()
+    {
+      return "Error you are not authorized!!!";
+    }
+  }
+}
--- CookieAuth/Startup.cs
+++ CookieAuth/Startup.cs
@@ -7,31 +7,47 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace CookieAuth
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+      services.AddMvc();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      services.AddAuthorization(options =>
+      {
+        options.AddPolicy("YouMayPass", policy => policy.RequireClaim("UserName"));
+        options.AddPolicy("YouMayNotPass", policy => policy.RequireClaim("Password"));
+      });
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseCookieAuthentication(new CookieAuthenticationOptions()
+      {
+        AuthenticationScheme = "TestInstance",
+        LoginPath = new PathString("/Home/Index"),
+        AccessDeniedPath = new PathString("/Home/FailedAccess"),
+        AutomaticAuthenticate = true,
+        AutomaticChallenge = true
+      });
+
+      app.UseMvcWithDefaultRoute();
+
+    }
+  }
 }
+
